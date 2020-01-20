6445b56483f16159a0012a4de2dc42dde9adce1c
cjasicki <cjasicki@gmail.com>
2020-01-19 22:53:03 -0600

    Add project files.

---
+++ Example5_CS/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Example5_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPayrate = new System.Windows.Forms.TextBox();
+            this.lblPayrate = new System.Windows.Forms.Label();
+            this.dgvPayInfo = new System.Windows.Forms.DataGridView();
+            this.cboEmployee = new System.Windows.Forms.ComboBox();
+            this.lblEmployee = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPayrate
+            // 
+            this.txtPayrate.Location = new System.Drawing.Point(530, 75);
+            this.txtPayrate.Name = "txtPayrate";
+            this.txtPayrate.Size = new System.Drawing.Size(100, 22);
+            this.txtPayrate.TabIndex = 3;
+            // 
+            // lblPayrate
+            // 
+            this.lblPayrate.Location = new System.Drawing.Point(425, 75);
+            this.lblPayrate.Name = "lblPayrate";
+            this.lblPayrate.Size = new System.Drawing.Size(100, 23);
+            this.lblPayrate.TabIndex = 2;
+            this.lblPayrate.Text = "Payrate:";
+            this.lblPayrate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // dgvPayInfo
+            // 
+            this.dgvPayInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPayInfo.Location = new System.Drawing.Point(100, 150);
+            this.dgvPayInfo.Name = "dgvPayInfo";
+            this.dgvPayInfo.RowHeadersWidth = 51;
+            this.dgvPayInfo.RowTemplate.Height = 24;
+            this.dgvPayInfo.Size = new System.Drawing.Size(600, 250);
+            this.dgvPayInfo.TabIndex = 4;
+            // 
+            // cboEmployee
+            // 
+            this.cboEmployee.FormattingEnabled = true;
+            this.cboEmployee.Location = new System.Drawing.Point(170, 75);
+            this.cboEmployee.Name = "cboEmployee";
+            this.cboEmployee.Size = new System.Drawing.Size(200, 24);
+            this.cboEmployee.TabIndex = 1;
+            this.cboEmployee.SelectionChangeCommitted += new System.EventHandler(this.CboEmployee_SelectionChangeCommitted_1);
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.Location = new System.Drawing.Point(65, 75);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(100, 23);
+            this.lblEmployee.TabIndex = 0;
+            this.lblEmployee.Text = "Employee:";
+            this.lblEmployee.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(50, 520);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(600, 23);
+            this.lblError.TabIndex = 5;
+            this.lblError.Text = "(error)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.txtPayrate);
+            this.Controls.Add(this.lblPayrate);
+            this.Controls.Add(this.dgvPayInfo);
+            this.Controls.Add(this.cboEmployee);
+            this.Controls.Add(this.lblEmployee);
+            this.Controls.Add(this.lblError);
+            this.Name = "Form1";
+            this.Text = "Stored Proc Output Parameter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPayrate;
+        internal System.Windows.Forms.Label lblPayrate;
+        internal System.Windows.Forms.DataGridView dgvPayInfo;
+        internal System.Windows.Forms.ComboBox cboEmployee;
+        internal System.Windows.Forms.Label lblEmployee;
+        internal System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ Example5_CS/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example5_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //*****
+        //** Form
+        //**
+        //*******************************************************
+        //** Procedure: Form1_Load()
+        //**   Initializes combobox
+        //*******************************************************
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            LoadEmployeeNames();
+        }
+
+        //ADD the selectionChangeCommitted EVENT to the cboEmployee control.
+        // comboBox
+        // Load payroll information and payrate for employee
+        private void cboEmployee_SelectionChangeCommitted(object Sender, EventArgs e)
+        {
+            lblError.Text = "";
+            if (cboEmployee.SelectedIndex >= 0)
+            {
+                DisplayEmployeePayroll(Convert.ToInt32(cboEmployee.SelectedValue));
+            }
+        }
+
+        //*****
+        //** Procedure
+        //*****
+        //*******************************************************
+        //** Procedure: LoadEmployeeNames()
+        //**   Populate combo box with employee name and id
+        //*******************************************************
+        private void LoadEmployeeNames()
+        {
+            DataSet dsData;
+
+            lblError.Text = "";
+
+            dsData = clsDatabase.GetEmployeeNames();
+            if(dsData== null)
+            {
+                lblError.Text = "Error retrieving employee names";
+            }
+            else if (dsData.Tables.Count <1)
+            {
+                lblError.Text = "Error retrieving employee names";
+                dsData.Dispose();
+            }
+            else
+            {
+                cboEmployee.DataSource = dsData.Tables[0];
+                cboEmployee.DisplayMember = "LName";
+                cboEmployee.ValueMember = "EmpID";
+
+                if(cboEmployee.Items.Count > 0)
+                {
+                    cboEmployee.SelectedIndex = 0;
+                    DisplayEmployeePayroll(Convert.ToInt32(cboEmployee.SelectedValue));
+                }
+            }
+
+        }
+        // Display payrate and payroll for specified employee
+        private void DisplayEmployeePayroll(Int32 intEmpID)
+        {
+            DataSet dsData;
+            Decimal decPayrate;
+
+            decPayrate = clsDatabase.GetEmployeePayrate(intEmpID);
+            if(decPayrate < 0m)
+            {
+                lblError.Text = "$0.00";
+            }
+            else
+            {
+                txtPayrate.Text = decPayrate.ToString("c");
+            }
+
+            dsData = clsDatabase.GetEmplyeePayroll(intEmpID);
+            if (dsData ==null)
+            {
+                lblError.Text = "Error retrieving payroll info";
+            }
+            else if(dsData.Tables.Count < 1)
+            {
+                lblError.Text = "Error retrieving payroll info";
+                dsData.Dispose();
+            }
+            else
+            {
+                dgvPayInfo.DataSource = dsData.Tables[0];
+            }
+        }
+
+  
+
+        private void CboEmployee_SelectionChangeCommitted_1(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            if (cboEmployee.SelectedIndex >= 0)
+            {
+                Int32 x = Convert.ToInt32(cboEmployee.SelectedValue);
+                DisplayEmployeePayroll(x);
+            }
+        }
+    }
+}
